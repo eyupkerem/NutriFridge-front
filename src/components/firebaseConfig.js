fa6f2bdffc0f5cb2807732f686de9938107c7a9d
eyupkerem <malkockerem1312@gmail.com>
2024-05-29 23:17:14 +0300

    first commit

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -3,12 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsEoH4q0q0xhRq-hkSAFmZh0iEja8m6zI",
-  authDomain: "nutrifridge.firebaseapp.com",
-  projectId: "nutrifridge",
-  storageBucket: "nutrifridge.appspot.com",
-  messagingSenderId: "781974439841",
-  appId: "1:781974439841:web:fbe223ba0295a500cf2de0"
+
 };
 
 const app = initializeApp(firebaseConfig);
